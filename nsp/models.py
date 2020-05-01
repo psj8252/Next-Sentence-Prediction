b28e11ccd8ac54f4e0e909c226d1b3365b30e13c
박상준 <psj8252@naver.com>
2020-05-01 17:53:15 +0900

    feat: Add save and load to model

--- nsp/models.py
+++ nsp/models.py
@@ -1,6 +1,26 @@
+import torch
 import torch.nn as nn
 
 
 class BaseModel(nn.Module):
-    def __init__(self):
-        super(BaseModel, self).__init__()
+    save_params = {}
+
+    def save(self, path):
+        """
+        Save model to file.
+        :param path: (str) Path to save model a file.
+        'save_params' is dict used as keyword arguments when loading the model.
+        """
+        torch.save({"save_params": self.save_params, "state_dict": self.state_dict()}, path)
+
+    @classmethod
+    def load(cls, path):
+        """
+        Load model from file.
+        :param path: (str) Path to load model from the file.
+        :return: (torch.nn) Loaded model.
+        """
+        checkpoint = torch.load(path)
+        model = cls(**checkpoint["save_params"])
+        model.load_state_dict(checkpoint["state_dict"])
+        return model

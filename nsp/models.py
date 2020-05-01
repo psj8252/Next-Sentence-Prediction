488cb8a765058f32a9efcf5eb3fb708728956e3b
박상준 <psj8252@naver.com>
2020-05-02 00:56:25 +0900

    feat: Implement transformer model

--- nsp/models.py
+++ nsp/models.py
@@ -24,3 +24,48 @@ class BaseModel(nn.Module):
         model = cls(**checkpoint["save_params"])
         model.load_state_dict(checkpoint["state_dict"])
         return model
+
+
+class TransformerModel(BaseModel):
+    def __init__(self, vocab_size, d_model, nhead, dim_feedforward, num_layers, dropout, activation):
+        """
+        :param vocab_size: the number of vocabulary words.
+        :param d_model: transformer model dimention.
+        :param nhead: the number of transformer head.
+        :param dim_feedforward: transformer feedfoward dimention.
+        :param num_layers: the number of transformer encoder layers.
+        :param dropout: transformer dropout.
+        :param activation: transformer activcation
+        """
+        super(TransformerModel, self).__init__()
+
+        self.embed = nn.Embedding(vocab_size, d_model)
+        encoder_layers = nn.TransformerEncoderLayer(d_model, nhead, dim_feedforward, dropout, activation)
+        self.transformer_encoder = nn.TransformerEncoder(encoder_layers, num_layers)
+        self.pooler = nn.Linear(d_model, 2)
+
+        # Set parameters for save
+        self.save_params = {
+            "vocab_size": vocab_size,
+            "d_model": d_model,
+            "nhead": nhead,
+            "dim_feedforward": dim_feedforward,
+            "num_layers": num_layers,
+            "dropout": dropout,
+            "activation": activation,
+        }
+
+    def forward(self, input_tokens):
+        """
+        :param input_tokens: (torch.tensor) input tokens. shaped (Sequence x BatchSize x VocabSize)
+        :return: (torch.tensor) model result shaped (BatchSize, 2)
+
+        [Shape]
+        embedded: (Sequence x BatchSize x d_model)
+        encoded: (Sequence x BatchSize x d_model)
+        pooled: (BatchSize, 2)
+        """
+        embedded = self.embed(input_tokens)
+        encoded = self.transformer_encoder(embedded)
+        pooled = self.pooler(encoded[0])
+        return pooled

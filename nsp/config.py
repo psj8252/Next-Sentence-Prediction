e880bde720c4626cabccf4e2a1edbaa7acecca71
박상준 <psj8252@naver.com>
2020-05-02 16:20:46 +0900

    feat: Add inference config

--- nsp/config.py
+++ nsp/config.py
@@ -58,3 +58,30 @@ class TrainConfig(NamedTuple):
 
         config.update(kwargs)
         return cls(**config)
+
+
+class InferConfig(NamedTuple):
+    """
+    model_type: Model type name in nsp.models ex) "TransformerModel"
+    model_path: The model file path that infer texts.
+    vocab_path: Vocab file path to numericalize tokens.
+    val_batch_size: batch size for inference.
+    """
+
+    model_type: str
+    model_path: str
+    vocab_path: str
+    val_batch_size: int
+
+    @classmethod
+    def load_from_json(cls, config_path, **kwargs):
+        """
+        Load config from the config file.
+        :param config_path: (str) yaml format config file.
+        :param **kwargs: parameters to override json setting.
+        """
+        with open(config_path) as f:
+            config = yaml.load(f, yaml.FullLoader)
+
+        config.update(kwargs)
+        return cls(**config)

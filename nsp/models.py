ff4140429a4dfb9072bd709af603bf4e15abdbef
박상준 <psj8252@naver.com>
2020-07-25 17:20:16 +0900

    update: Use loss of context, query cossim

--- nsp/models.py
+++ nsp/models.py
@@ -28,7 +28,18 @@ class BaseModel(nn.Module):
 
 
 class TransformerModel(BaseModel):
-    def __init__(self, vocab_size, d_model, nhead, dim_feedforward, num_layers, embedding_dim, dropout, activation):
+    def __init__(
+        self,
+        vocab_size,
+        d_model,
+        nhead,
+        dim_feedforward,
+        num_layers,
+        embedding_dim,
+        dropout,
+        cossim_threshold,
+        activation,
+    ):
         """
         :param vocab_size: the number of vocabulary words.
         :param d_model: transformer model dimention.
@@ -37,6 +48,7 @@ class TransformerModel(BaseModel):
         :param num_layers: the number of transformer encoder layers.
         :param embedding_dim: the dimention to embed context, query, reply.
         :param dropout: transformer dropout.
+        :param cossim_threshold: if cossim is bigger that threshold, it is considered as positive prediction.
         :param activation: transformer activcation
         """
         super(TransformerModel, self).__init__()
@@ -48,6 +60,7 @@ class TransformerModel(BaseModel):
         self.query_ffn = nn.Linear(d_model, embedding_dim)
         self.reply_ffn = nn.Linear(d_model, embedding_dim)
         self.context_query_mean_ffn = nn.Linear(d_model, embedding_dim)
+        self.cossim_threshold = cossim_threshold
 
         # Set parameters for save
         self.save_params = {
@@ -58,6 +71,7 @@ class TransformerModel(BaseModel):
             "num_layers": num_layers,
             "embedding_dim": embedding_dim,
             "dropout": dropout,
+            "cossim_threshold": cossim_threshold,
             "activation": activation,
         }
 
@@ -88,29 +102,38 @@ class TransformerModel(BaseModel):
         context_query = self.context_query_mean_ffn((context + query) / 2)
 
         # Normalize
+        context = F.normalize(context, p=2, dim=1)
+        query = F.normalize(context, p=2, dim=1)
+        reply = F.normalize(context, p=2, dim=1)
         context_query = F.normalize(context_query, p=2, dim=1)
-        reply = F.normalize(reply, p=2, dim=1)
 
-        output = torch.matmul(context_query, reply.T)
-        return output
+        # Calculate Cos Similarities
+        context_score = torch.matmul(context, reply.T)
+        query_score = torch.matmul(query, reply.T)
+        context_query_score = torch.matmul(context_query, reply.T)
 
-    def to_labels(self, output):
+        return context_score, query_score, context_query_score
+
+    def to_labels(self, outputs):
         """
-        Return to labels (ex [1, 0, 1, 1, 0, ...]) from model output
+        Return to labels (ex [1, 0, 1, 1, 0, ...]) from model context & query output
         """
-        return (output.diagonal() > 0.5).cpu().detach().numpy()
+        return (outputs[2].diagonal() > self.cossim_threshold).cpu().detach().numpy()
 
-    def criterion(self, output):
-        mask_for_positive = 1 - torch.eye(*output.shape, device=output.device)
-        positive_score = output.diagonal().sum()
-        negative_score = (output.exp() * mask_for_positive).sum(dim=1).log().sum()
-        score = positive_score - negative_score
-        return -score
+    def criterion(self, outputs):
+        loss = 0.0
 
-    def metrics_parts(self, output):
-        n_positive = output.shape[0]
-        answer = torch.zeros_like(output).fill_diagonal_(1)
-        is_correct = (output >= 0.5) == answer
+        for output in outputs:
+            mask_for_positive = 1 - torch.eye(*output.shape, device=output.device)
+            positive_score = output.diagonal().sum()
+            negative_score = (output.exp() * mask_for_positive).sum(dim=1).log().sum()
+            loss -= positive_score - negative_score
+        return loss
+
+    def metrics_parts(self, outputs):
+        n_positive = outputs[2].shape[0]
+        answer = torch.zeros_like(outputs[2]).fill_diagonal_(1)
+        is_correct = (outputs[2] >= self.cossim_threshold) == answer
 
         TP = is_correct.diagonal().sum().cpu().detach().item()
         TN = is_correct.sum().cpu().detach().item() - TP
